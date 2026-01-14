82cab322776efef098d2be9fdd2aecce53a654c0
sadimanna <manna.siladittya@gmail.com>
2026-01-15 00:46:36 +0530

    april primary proto

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import torch
 DATASETS = ["cifar10", "cifar100", "stl10", "tiny_imagenet", "imagenet"]
 DEFAULT_DATASET = "cifar10"
 DEFAULT_MODEL = "vit_tiny_patch16_224"
-DEFAULT_BATCH_SIZE = 1
+DEFAULT_BATCH_SIZE = 4
 DEFAULT_EPOCHS = 10
 DEFAULT_LR = 1e-3
 DEFAULT_DEVICE = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
